25d8523b950524f63e4f0abcb21b500e6281eddb
Md. Ibrahim Hossain <ih75754@gmail.com>
2022-04-04 16:56:49 +0600

    router setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
+import Packages from './components/Packages/Packages';
+import Blog from './components/Blog/Blog';
+import Header from './components/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-    </div>
+    <Router >
+      <Header></Header>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route exact path="/home">
+              <Home></Home>
+            </Route>
+            <Route exact path="/blog">
+              <Blog></Blog>
+            </Route>
+            <Route exact path="/packages">
+              <Packages></Packages>
+            </Route>
+          </Switch>
+        </Router>
+  </div>
   );
 }
 
--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
@@ -6,20 +7,20 @@ const Header = () => {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container">
-    <a className="navbar-brand" href="#"><h4 classNameName='text-warning'>Holidays</h4></a>
+    <NavLink className="navbar-brand text-warning" to="/"><h4 >Holidays</h4></NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarText">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <NavLink className="nav-link active" aria-current="page" to="/home">Home</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Packages</a>
+          <NavLink className="nav-link" to="packages">Packages</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Blog</a>
+          <NavLink className="nav-link" to="blog">Blog</NavLink>
         </li>
       </ul>
       <span className="navbar-text">
