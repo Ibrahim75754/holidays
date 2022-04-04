570eb36a1779bc1d6a1f7034ac6ee9002c94ffe7
Md. Ibrahim Hossain <ih75754@gmail.com>
2022-04-04 13:18:11 +0600

    first commit

---
+++ src/components/Home/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div className="banner text-white text-center">
+            <div className="dark-overly text-position">
+                <div className="">
+                <h4 className='text-warning'>Lets go now</h4>
+                    <h1 className='text-size'>TRAVELING AROUND THE WORLD </h1>
+                    <h4>With Holidays</h4>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
