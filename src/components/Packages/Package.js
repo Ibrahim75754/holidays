c21df03dfb8c7647eab469e8b045e5541fdbb854
Md. Ibrahim Hossain <ih75754@gmail.com>
2022-04-05 13:27:43 +0600

    packages page design

---
+++ src/components/Packages/Package.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Package.css';
+
+const Package = (props) => {
+    const { _id, name, img, description, price, duration } = props.Package;
+    return (
+        <div>
+            <div className="col">
+                <div className="card p-2">
+                    <img src={img} className="card-img-top img-fluid" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{name}</h5>
+                        <p className="card-text">{description}</p>
+                        <h4>Offering price: {price} BDT</h4>
+                        <p>Duration: {duration}</p>
+                        <NavLink to={`/home/${_id}`} className="btn btn-color py-2 px-5">Book Now</NavLink>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Package;
--- src/components/Packages/Packages.js
+++ src/components/Packages/Packages.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import Footer from '../Shared/Footer/Footer';
 import Header from '../Shared/Header/Header';
+import Package from './Package';
 
 const Packages = () => {
+    const [packages,setPackages]=useState([]);
+    useEffect(()=>{
+        fetch('')
+        .then(res => res.json())
+        .then(data => setPackages(data));
+    },[]);
+
     return (
         <div>
             <Header></Header>
-            <h1>packages page</h1>
+            <div className="container py-5">
+            <div className="text-center pb-4">
+                <h1>POPULAR PACKAGES</h1>
+                <p>The practice involves swishing oil around in your mouth to remove bacteria, which can turn into plaque and cause your teeth to look yellow.</p>
+            </div>
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+                {
+                    packages.map(pac => <Package
+                        key={pac._id}
+                        pac={pac}
+                    ></Package>)
+                }
+            </div>
+        </div>
             <Footer></Footer>
         </div>
     );
