ab35870010323151b806d4b5818c1ebfa8dce035
Md. Ibrahim Hossain <ih75754@gmail.com>
2022-04-05 12:54:51 +0600

    contact us section add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Footer from '../Shared/Footer/Footer';
 import Header from '../Shared/Header/Header';
 import Banner from './Banner';
+import ContactUs from './ContactUs';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
            <Banner></Banner>
+           <ContactUs></ContactUs>
            <Footer></Footer>
         </div>
     );
