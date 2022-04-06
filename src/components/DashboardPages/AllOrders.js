9d77aef495e70c55fd4c551cf58e93cf376d4623
Md. Ibrahim Hossain <ih75754@gmail.com>
2022-04-06 15:55:39 +0600

    myOrders and AllOrders page dynamic

--- src/components/DashboardPages/AllOrders.js
+++ src/components/DashboardPages/AllOrders.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 
 const AllOrders = () => {
+    const [packages, setPackages] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/myOrders')
+            .then(res => res.json())
+            .then(data => setPackages(data));
+    }, []);
+    let Id = 1;
+
+    const handleDelete = id => {
+        const areUsure = window.confirm('Are You Sure, Want To Delete?');
+        if (areUsure) {
+            fetch(`/myOrders/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount) {
+                        alert('Delete Successful');
+                        const remaining = packages.filter(pac => pac._id !== id);
+                        setPackages(remaining);
+                    }
+                })
+        }
+    }
     return (
         <div>
-            <h1>All Orders page</h1>
+           
+            <h2 className="text-center my-4">All order list :</h2>
+            <div className="table-responsive">
+                <table class="table table-hover text-center">
+                    <thead>
+                        <tr>
+                            <th scope="col">serial</th>
+                            <th scope="col">PackageId No.</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">Address</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            packages.map(pac => <tr>
+                                <th scope="row">{Id++}</th>
+                                <td>{pac.packageId}</td>
+                                <td>{pac.name}</td>
+                                <td>{pac.email}</td>
+                                <td>{pac.phone}</td>
+                                <td>{pac.address}</td>
+                                <td><button onClick={() => handleDelete(pac._id)} className="btn btn-danger">Delete Order</button></td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/components/DashboardPages/MyOrders.js
+++ src/components/DashboardPages/MyOrders.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../hooks/useAuth';
 
 const MyOrders = () => {
+    const { user } = useAuth();
+
+    const [packages, setPackages] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/myOrders')
+            .then(res => res.json())
+            .then(data => setPackages(data));
+    }, []);
+    const myOrders = packages.filter(pac => pac.email === user.email);
+    console.log(myOrders)
+    let Id = 1;
+
+    const handleDelete = id => {
+        const areUsure = window.confirm('Are You Sure, Want To Delete?');
+        if (areUsure) {
+            fetch(`/myOrders/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount) {
+                        alert('Delete Successful');
+                        const remaining = packages.filter(pac => pac._id !== id);
+                        setPackages(remaining);
+                    }
+                })
+        }
+    }
+
     return (
         <div>
-            <h1>my orders</h1>
+            <h1 className="text-center my-4">My Orders List</h1>
+            <div className="table-responsive">
+                <table class="table table-hover text-center">
+                    <thead>
+                        <tr>
+                            <th scope="col">serial</th>
+                            <th scope="col">PackageId No.</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">Address</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            myOrders.map(pac => <tr>
+                                <th scope="row">{Id++}</th>
+                                <td>{pac.packageId}</td>
+                                <td>{pac.name}</td>
+                                <td>{pac.email}</td>
+                                <td>{pac.phone}</td>
+                                <td>{pac.address}</td>
+                                <td><button onClick={() => handleDelete(pac._id)} className="btn btn-danger">Delete Order</button></td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
